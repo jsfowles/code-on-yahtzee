1f9d3951efeca2bfcef238f018b89d35aa4758c4
Jacob Fowles <js.fowles@gmail.com>
2017-05-05 10:58:27 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Hello from './Hello';
+
+class App extends Component {
+  state = { counter: 0};
+
+  increment = () => {
+    this.setState({ counter: this.state.counter + 1});
+    console.log(this.state.counter);
+  }
+
+  decrement = () => {
+    this.setState({ counter: this.state.counter - 1});
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{ this.state.counter }</h1>
+        <button onClick={ this.increment }>+</button>
+        <button onClick={ this.decrement }>-</button>
+      </div>
+    );
+  }
+}
+
+export default App;
